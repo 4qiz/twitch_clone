72649ced20e3d06a098d1ae4e6b37ae9923e8fd9
EgorPC <egor.antipin.1995mail.ru@gmail.com>
2024-04-12 19:07:39 +0300

    add navbar

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -6,8 +6,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="flex flex-col gap-y-4">
-      <h1>Dashboard</h1>
-      <UserButton afterSignOutUrl="/" />
+      <h1>home page</h1>
     </div>
   );
 }
