ac9810376cffaa489f5275754cff97d316730f05
EgorPC <egor.antipin.1995mail.ru@gmail.com>
2024-04-15 18:48:15 +0300

    fix recommended list

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -4,18 +4,16 @@ import { useSidebar } from "@/store/use-sidebar";
 import { use, useEffect, useState } from "react";
 import { ToggleSkeleton } from "./toggle";
 import { RecommendedSkeleton } from "./recommended";
+import { useIsClient } from "usehooks-ts";
 
 interface WrapperProps {
   children: React.ReactNode;
 }
 
 export const Wrapper = ({ children }: WrapperProps) => {
-  const [isClient, setIsClient] = useState(false);
+  const isClient = useIsClient();
   const { collapsed } = useSidebar((state) => state);
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
   if (!isClient)
     return (
       <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2d2e35] z-50">
